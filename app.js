1c227ba0b900913bcf9033f6cd1db54f9ea383c5
shrishantgit <karkhelikarshrishant@gmail.com>
2026-06-22 00:10:21 +0530

    feat: broadcast live locations and handle disconnections

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const path = require("path");
-const socketio = require("socket.io");
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+// View engine and static files setup
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
-io.on("connection", function (socket) {
-    console.log("Connected:", socket.id);
+io.on('connection', function (socket) {
+    console.log("connected");
+
+    // Receive the location from the frontend and broadcast to everyone else
+    socket.on('send-location', function (data) {
+        io.emit('receive-location', { id: socket.id, ...data });
+    });
+
+    // Handle user disconnect
+    socket.on('disconnect', function () {
+        io.emit('user-disconnected', socket.id);
+    });
 });
 
-app.get("/", function (req, res) {
-    res.render("index");
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+server.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
--- public/js/script.js
+++ public/js/script.js
@@ -27,3 +27,25 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 const markers = {};
+
+// Handle incoming locations
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude], 19);
+
+    if (markers[id]) {
+        // If marker already exists, just update position
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        // Otherwise, create a new marker and add it
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+// Handle disconnected users
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});  
